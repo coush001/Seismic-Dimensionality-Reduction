f0924a6215dcfce39870c9c4ba713332431ed6b6
coush001 <coush001@users.noreply.github.com>
2019-06-06 13:13:08 +0100

    No known changes

--- Project-Files/main.py
+++ Project-Files/main.py
@@ -3,15 +3,26 @@ import umap
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import pandas as pd
-import numpy as np
-import datashader as ds
-import datashader.transfer_functions as tf
-from datashader.bokeh_ext import InteractiveImage
 import bokeh.plotting as bp
 import segypy
 from scipy.interpolate import interp2d
 from utils import load_seismic, load_horizon, colorbar, interpolate_horizon, plot_section_horizon_and_well
 from utils import flatten_on_horizon
+
+import argparse
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.autograd import Variable
+import torch.nn as nn
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+from torch.utils.data import TensorDataset
+from sklearn.model_selection import ShuffleSplit
+from utils import VAE, set_seed
+import os
+from livelossplot import PlotLosses
 
 print("works")
 
@@ -33,6 +44,15 @@ class MasterClass:
 
         self.near_traces = []
         self.far_traces = []
+
+        self.near_traces_emb = []
+        self.far_traces_emb = []
+
+        self.stacked = np.empty(0)
+
+        self.FF = np.empty(0)
+
+        self.embedding_stack_ff = []
 
     def load_near(self, near_sgy):
         self.near_stack_amplitudes, self.twt_n = load_seismic(near_sgy, inlines=[1300, 1502, 2], xlines=[1500, 2002, 2])
@@ -89,28 +109,80 @@ class MasterClass:
         self.near_traces /= well_variance_near
         self.far_traces /= well_variance_far
 
-        self.near_traces_emb = self.near_traces.reshape(-1, 64)
+        self.near_traces_emb = self.near_traces.reshape(-1, 64)  # 2d arrays of traces
         self.far_traces_emb = self.far_traces.reshape(-1, 64)
+
+    def stack_traces(self, near=None, far=None):
+        if not near:
+            near = self.near_traces_emb
+        if not far:
+            far = self.far_traces_emb
+        self.stacked = np.concatenate([near, far], 1)
+
+    def get_FF(self, plot=True):
+        x_avo = self.near_traces_emb
+        y_avo = self.far_traces_emb - self.near_traces_emb
+
+        lin_reg = LinearRegression(fit_intercept=False, normalize=False, copy_X=True, n_jobs=1)
+        lin_reg.fit(x_avo.reshape(-1, 1), y_avo.reshape(-1, 1))
+
+        print("Linear Regression coefficient: %1.2f" % lin_reg.coef_[0, 0])
+        self.FF = y_avo - lin_reg.coef_ * x_avo
+
+        if plot:
+            fig, ax = plt.subplots(1, 1, figsize=(12, 12))
+            sc = ax.imshow(np.min(self.FF.reshape(self.near_traces.shape[0],
+                                                  self.near_traces.shape[1], 64)[:, :, :], 2))
+            colorbar(sc)
+
+    def umap(self):
+        print(type(self.stacked), type(self.FF), self.stacked.shape, self.FF.reshape(-1, 64).shape)
+        self.embedding_stack_ff = umap.UMAP(n_neighbors=50,
+                                       min_dist=0.001,
+                                       metric='correlation',
+                                       verbose=True,
+                                       random_state=42).fit_transform(np.concatenate([self.stacked,
+                                                                                      self.FF.reshape(-1, 64)], 1))
+
+    def plot_cluster(self, overlay=None):
+        if not overlay:
+            overlay = self.FF
+        fig, ax = plt.subplots(1, 1, figsize=(12, 12))
+        sc = ax.scatter(self.embedding_stack_ff[::, 0], self.embedding_stack_ff[::, 1], s=2.0, c=np.min(overlay, 1)[::])
+        colorbar(sc)
+        plt.show()
+
 
 
 test = MasterClass('TEST_OBJECT')
 
-# Loading Data
-test.load_near('../data/3d_nearstack.sgy')
-test.load_far('../data/3d_farstack.sgy')
-test.load_horizon('../data/Top_Heimdal_subset.txt')
-
-test.plot_horizon()
-
-# add well location
-test.add_well('well_1', 36, 276//2)
-
-# print well dictionary and plot
-print("Dictionary of wells", test.wells)
-test.plot_horizon_well('well_1')
-
-# flatten
-test.flatten()
-
+# # Loading Data
+# test.load_near('../data/3d_nearstack.sgy')
+# test.load_far('../data/3d_farstack.sgy')
+# test.load_horizon('../data/Top_Heimdal_subset.txt')
 #
+# test.plot_horizon()
+#
+# # add well location
+# test.add_well('well_1', 36, 276//2)
+#
+# # print well dictionary and plot
+# print("Dictionary of wells", test.wells)
+# test.plot_horizon_well('well_1')
+#
+# # flatten
+# test.flatten_traces()
+
+# normalise and reshape to 2d arrays
+test.normalise_from_well()
+
+# linear regression to find FF:
+test.get_FF()
+
+# Stack the near and far on top of eachother
+test.stack_traces()
+
+# Umap dimensionality reduction:
+test.umap()
+test.plot_cluster()
 
--- Project-Files/test_1.py
+++ Project-Files/test_1.py
@@ -1,9 +1,4 @@
 import pytest
 
 
-def fun():
-    return 4
 
-
-def test_fun():
-    assert fun() == 4
